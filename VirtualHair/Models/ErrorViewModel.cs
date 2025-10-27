c75a595838fa90293d7d09e667dcb3b637d14121
HamidHamidd <xamid.alii@gmail.com>
2025-10-27 18:13:15 +0200

    Add project files.

---
+++ VirtualHair/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace VirtualHair.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
